21243816804f7afa6bb58d891c3eac1e1b198ae1
Key-Maker77 <rifqi153syah@gmail.com>
2023-08-25 20:55:37 +0700

    edit home blade

--- resources/views/layouts/login/app.blade.php
+++ resources/views/layouts/login/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,13 +9,15 @@
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" rel="stylesheet">
     <link rel="icon" type="image/png" sizes="16x16" href="{{URL::asset('images/logo.jpg')}}">
     <style type="text/css">
-        .body{
+        .body {
             background-color: #0087CC;
         }
     </style>
 </head>
+
 <body class="body">
     @yield('content')
 
 </body>
+
 </html>
